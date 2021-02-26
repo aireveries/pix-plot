cf59115263ba0bb066ac1d87f4522a6563b18d21
Patrick Rodriguez <patrick.rodriguez@aireverie.com>
2021-02-26 23:24:07 +0000

    update extraction scripts

--- extract_objects_xml.py
+++ extract_objects_xml.py
@@ -5,6 +5,8 @@ import glob
 from PIL import Image
 import xml.etree.ElementTree as ET
 import numpy as np
+from pathlib import Path
+from tqdm import tqdm
 
 # Just try to open image and parse it using the xml
 path = sys.argv[1]
@@ -13,13 +15,17 @@ writer = csv.writer(csvfile, delimiter=",", quotechar="|",
                     quoting=csv.QUOTE_MINIMAL)
 writer.writerow(['filename', 'tags', 'description', 'permalink'])
 
-for fileName in glob.glob(path):
+Path('./extracted_objects').mkdir(exist_ok=True)
+
+for fileName in tqdm(glob.glob(path), 'extracting objects'):
     if fileName[-8:] != "mask.png":
         imFile = fileName
-        dataFile = imFile[:-4] + ".xml"
         im = Image.open(imFile)
         pix = np.asarray(im)
         # Have to parse the proper mask
+        dataFile = Path(imFile[:-4] + ".xml")
+        if not dataFile.exists():
+            dataFile = Path(imFile[:-4].replace('image', 'annotation') + '.xml')
         tree = ET.parse(dataFile)
         root = tree.getroot()
 
@@ -43,6 +49,6 @@ for fileName in glob.glob(path):
             # some images can be outside of screen, in which case, drop em:
             if subImageArray.shape[0] > 0 and subImageArray.shape[1] > 0:
                 newim = Image.fromarray(subImageArray)
-                newim.save("extractedobjects/" + outfile, "png")
+                newim.save("extracted_objects/" + outfile, "png")
                 writer.writerow([outfile, tag, persistentID, "n/a"])
                 # might also want to potentially write metadata file. Will do this later
